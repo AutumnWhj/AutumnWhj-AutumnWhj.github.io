fe590ae326cc60694f25fb573cc7b12634ef637d
AutumnWhj <39156049+AutumnWhj@users.noreply.github.com>
2022-12-02 14:52:01 +0000

    Deploying to gh-pages from @ AutumnWhj/AutumnWhj.github.io@20e44a34e9796af2d9c18b2f2bb22a7025411529 🚀

--- js/global.js
+++ js/global.js
@@ -16,11 +16,11 @@
 // }, 4000);
 // alert("2");
 // 监听网络请求情况 如果在5秒内没有加载完毕 那么就会跳转到备用网址
-setInterval(function () {
-  if (document.readyState != "complete") {
-    window.location.href = `https://ivwv.netlify.app${window.location.pathname}`;
-  }
-}, 5000);
+// setInterval(function () {
+//   if (document.readyState != "complete") {
+//     window.location.href = `https://ivwv.netlify.app${window.location.pathname}`;
+//   }
+// }, 5000);
 
 // 动态小人物 女
 
@@ -29,8 +29,8 @@ L2Dwidget.init({
   model: {
     jsonPath:
       // "https://unpkg.com/live2d-widget-model-shizuku@1.0.5/assets/shizuku.model.json",
-      // 'https://unpkg.com/live2d-widget-model-koharu@1.0.5/assets/koharu.model.json',
-      "https://unpkg.com/live2d-widget-model-miku@1.0.5/assets/miku.model.json",
+      'https://unpkg.com/live2d-widget-model-koharu@1.0.5/assets/koharu.model.json',
+      // "https://unpkg.com/live2d-widget-model-miku@1.0.5/assets/miku.model.json",
     scale: 1,
   },
   display: {
